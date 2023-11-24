cff9f563da0a97a88fdd0e8876ef873a367d8b0b
Anamaria <anarigu_170@hotmail.com>
2023-11-24 11:00:34 -0500

    modifications

--- Formulaires/index.php
+++ Formulaires/index.php
@@ -37,7 +37,7 @@ https://templatemo.com/tm-569-edu-meeting
       <div class="row">
         <div class="col-lg-8 col-sm-8">
           <div class="left-content">
-            <p>This is an educational <em>HTML CSS</em> template by TemplateMo website.</p>
+            <p>Ceci est un site <em>HTML CSS</em> dédié aux femmes!</p>
           </div>
         </div>
         <div class="col-lg-4 col-sm-4">
@@ -62,23 +62,23 @@ https://templatemo.com/tm-569-edu-meeting
                   <nav class="main-nav">
                       <!-- ***** Logo Start ***** -->
                       <a href="index.html" class="logo">
-                          Edu Meeting
+                            Magasin RIVERANIS
                       </a>
                       <!-- ***** Logo End ***** -->
                       <!-- ***** Menu Start ***** -->
                       <ul class="nav">
-                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                          <li><a href="meetings.html">Meetings</a></li>
-                          <li class="scroll-to-section"><a href="#apply">Apply Now</a></li>
+                          <li class="scroll-to-section"><a href="#top" class="active">Accueil</a></li>
+                          <li><a href="accueil.html">Accueil</a></li>
+                          <li class="scroll-to-section"><a href="#apply">Des offres</a></li>
                           <li class="has-sub">
-                              <a href="javascript:void(0)">Pages</a>
+                              <a href="javascript:void(0)">Enregistrer</a>
                               <ul class="sub-menu">
-                                  <li><a href="meetings.html">Upcoming Meetings</a></li>
-                                  <li><a href="meeting-details.html">Meeting Details</a></li>
+                                  <li><a href="meetings.html">Commencer la session</a></li>
+                                  <li><a href="meeting-details.html">Inscription</a></li>
                               </ul>
                           </li>
                           <li class="scroll-to-section"><a href="#courses">Courses</a></li> 
-                          <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
+                          <li class="scroll-to-section"><a href="#contact">Contactez-nous</a></li> 
                       </ul>        
                       <a class='menu-trigger'>
                           <span>Menu</span>
@@ -102,11 +102,11 @@ https://templatemo.com/tm-569-edu-meeting
             <div class="row">
               <div class="col-lg-12">
                 <div class="caption">
-              <h6>Hello Students</h6>
-              <h2>Welcome to Education</h2>
-              <p>This is an edu meeting HTML CSS template provided by <a rel="nofollow" href="https://templatemo.com/page/1" target="_blank">TemplateMo website</a>. This is a Bootstrap v5.1.3 layout. The video background is taken from Pexels website, a group of young people by <a rel="nofollow" href="https://www.pexels.com/@pressmaster" target="_blank">Pressmaster</a>.</p>
+              <h6>Salut les filles</h6>
+              <h2>Bienvenue à RIVERANIS</h2>
+              <p>Ceci est un site conçu pour vous !. Profitez de nos produits et de nos promotions, spécialement conçus pour vous !</p>
               <div class="main-button-red">
-                  <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
+                  <div class="scroll-to-section"><a href="#contact">Rejoignez-nous!</a></div>
               </div>
           </div>
               </div>
