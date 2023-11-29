045e0f2f595e4b42c7fa2515a723ea9b0e3e7397
Anamaria <anarigu_170@hotmail.com>
2023-11-29 13:15:25 -0500

    desing final page principal

--- Formulaires/index.php
+++ Formulaires/index.php
@@ -5,6 +5,7 @@
 <title>Magasin · Riveranis</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
+<form method="post" action="../Formulaires/result.php">
 <link href="../CSS/page_index.css" rel="stylesheet"/>
 </head>
 <body>
@@ -22,47 +23,48 @@
          <li><a href="#">Maquillage</a></li>
          <li><a href="#">Crèmes</a></li>
          <li><a href="#">Accessoires</a></li>
-         <li><a href="#">Contactez-nous</a></li>
+         <li><a href="#">Login!</a></li>
         </ul> 
     </div>
     <p>&nbsp;</p>
     <h1>Bienvenue à Notre Magasin</h1>
-    <div class="templatemo_text_area"><img src="../Images/templatemo_pic.jpg" alt="templatemo.com" width="192" height="147" />
+    <div class="templatemo_text_area"><img src="https://m.media-amazon.com/images/I/71Ty2K9bvNL._AC_UY200_.jpg" alt="templatemo.com" width="192" height="147" />
+      <p>Nous sommes une entreprise créée depuis 2020 par des femmes et pour des femmes, dans le but de mettre en valeur la beauté de chacune !</p>
       <p>Ceci est un site conçu pour vous !. Profitez de nos produits et de nos promotions, spécialement conçus pour vous !</p>
       <p>Vous trouverez ici les meilleurs produits de beauté pour que vous vous sentiez plus belle que jamais.</p>
     </div>
     <div id="templatemo_bottom">
       <div class="templatemo_mid1">
-        <h2>Featured Web Links</h2>
-        <p>
-        <a href="#">CSS Layouts</a><br />
-        <a href="#">Website Design</a><br />
-        <a href="#">Customer Services</a><br />
-        <a href="#">Testimonials</a><br />
-        <a href="#">Blogging Tips</a><br />
-        <a href="#">Photoshop Techniques</a><br />
-        <a href="#">Vector Illustrations</a></p>
+        <h2>Rejoignez-nous</h2>
+        <p>Si vous souhaitez faire partie de notre communauté et acheter nos produits, inscrivez-vous ici :</p>
+        <br></br>
+        <input name="nom" type="text" id="nom" placeholder="Entrez votre nom...*" required="">
+        <input name="prenom" type="text" id="prenom" placeholder="Entrez votre prénom...*" required="">
+        <br></br>
+        <input name="userid" type="text" id="userid" placeholder="Entrez votre utilisateur...*" required="">
+        <input name="motdepasse" type="password" id="motdepasse" placeholder="Entrez votre mot de passé...*" required="">
+        <br></br>
+        <input name="cmotdepasse" type="password" id="cmotdepasse" placeholder="Confirmation mot de passé...*" required="">
+        <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Entrez votre email...*" required="">
+        <br></br>
+        <input name="adresse" type="text" id="adresse" placeholder="votre adresse...*" required="">
+        <br></br>
+        <button type="submit" class="boton" id="form-submit">ENVOYER</button>
         <p>&nbsp;</p>
-      </div>
-      <div class="templatemo_left_border"></div>
-      <div class="templatemo_mid2">
-      <h2>Latest News</h2>
-      <p><strong>Post Date: 19-10-2020 </strong><br />
-        Maecenas aliquam, ligula id egestas suscipit, nisi sapien dignissim nibh, ac vestibulum lorem urna in neque. Aenean eget tortor eget ipsum aliquet porta. Morbi nunc. Praesent varius egestas velit. Praesent vel diam. Cras sed leo tempor neque placerat pretium. </p>
       </div>
       <div class="templatemo_right_border"></div>
 	   <div class="templatemo_mid3">
-         <p><strong>  Quick Contact</strong><br />
-           Tel: 000-100-1000<br />
+         <p><strong>  Contactez-nous</strong><br />
+           Tel: 819-100-1000<br />
            Fax: 001-001-0011<br />
-           Email: info {at} company.com</p>
+           Email: magriveranis@gmail.com</p>
          <p><br />
          <a href="http://validator.w3.org/check?uri=referer"><img height="31" alt="Valid XHTML 1.0 Transitional" src="http://www.w3.org/Icons/valid-xhtml10" width="88" vspace="8" border="0" /></a> <a href="http://jigsaw.w3.org/css-validator/check/referer"><img alt="Valid CSS!" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" vspace="8" border="0" /></a></p>
        </div>
     </div>
     <p>&nbsp;</p>
   </div>
-    <div id="templatemo_footer">Copyright © 2008 <a href="#">Your Company Name</a> 
+    <div id="templatemo_footer">Copyright © 2023 <a href="#">Magazin Riveranis</a> 
     <!-- Credit: www.templatemo.com --></div>
 </div>
 <!-- templatemo 022 floral -->
